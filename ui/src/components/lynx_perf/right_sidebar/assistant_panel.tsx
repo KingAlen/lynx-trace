487cd39e87a49fa0b433a9ea4d15da9bdf9823cd
KingAlen <19606165+KingAlen@users.noreply.github.com>
2025-10-20 11:58:34 +0800

    [BugFix] AI analysis is only available for version 3.4 and above.

--- ui/src/components/lynx_perf/right_sidebar/assistant_panel.tsx
+++ ui/src/components/lynx_perf/right_sidebar/assistant_panel.tsx
@@ -77,7 +77,7 @@ export class TraceAssistantPanel extends Component<{}, TraceAssistantPanelState>
     const isLynxVersionValid = await this.validateLynxVersion();
     if (!isLynxVersionValid) {
       this.setState({
-        validationError: 'Use Lynx SDK version 3.6.3 or above to enable AI analysis.',
+        validationError: 'Use Lynx SDK version 3.4 or above to enable AI analysis.',
         isValidationPassed: false
       });
       return;
@@ -349,7 +349,7 @@ export class TraceAssistantPanel extends Component<{}, TraceAssistantPanelState>
     }
     const result = await engine.query(`select args.display_value from slice join args on args.arg_set_id=slice.arg_set_id where slice.name='LynxEngineVersion' and args.key='debug.version'`);
     const version = result.numRows() > 0 ? result.firstRow({display_value: STR}).display_value : '';
-    return version >= '3.4.3';
+    return version >= '3.4';
   };
 
   validateLLMConfig = (): boolean => {
